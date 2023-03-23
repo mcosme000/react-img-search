95efe842c093ac025a6a9c582fb00b57d4065d5c
Maria Cosmen <mcosme00@gmail.com>
2023-03-23 17:30:48 +0900

    Add fetch method for API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+  const [word, setWord] = useState("")
+
+  const getWord = (e) => {
+    setWord(e.target.innerHTML)
+    console.log(e.target.innerHTML)
+    getImages()
+  }
+
+  const getImages = () => {
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${word}`, {
+    headers: {
+			"Authorization": "Client-ID Lj08PBsPio_7BT8J_8rAuGEt_3Dem1rgeiNB_73i_CY"
+		},
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => console.log(data))
+  }
+
   return(
     <div className="app">
       <p>App</p>
+      <p onClick={getWord}>deer</p>
+      <p onClick={getWord}>cat</p>
+      <p onClick={getWord}>bird</p>
+      <p onClick={getWord}>dog</p>
     </div>
   )
 }
