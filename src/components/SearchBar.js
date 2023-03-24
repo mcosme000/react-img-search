424ac71d400a9dfd4eb6ca92da3887731b56d76e
Maria Cosmen <mcosme00@gmail.com>
2023-03-24 14:56:30 +0900

    Get form input on change event

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = ({ onClick }) => {
+  const [ input, setInput ] = useState("")
 
-  const getInput = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    
-    onClick();
+    onClick(input);
+  }
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
   }
 
   return(
     <div>
-      <form>
-        <input type="text" id="input"/>
-        <button onClick={getInput}>Search images</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" id="input" onChange={handleChange}/>
+        <input type="submit" value="Search Images"/>
       </form>
     </div>
   )
 }
 
 export default SearchBar;
+
+// I am getting the input value from this component
+// and sending it back to the parent component
+// through the callback function that I received as a prop
